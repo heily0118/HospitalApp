af76f5d3e10054b72abb225c56fb15e304b74601
María Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2025-04-14 16:17:24 -0500

    Se creó el paquete models

--- src/autonoma/hospitalapp/main/HospitalApp.java
+++ src/autonoma/hospitalapp/main/HospitalApp.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package hospitalapp;
+package autonoma.hospitalapp.main;
 
 /**
  *
@@ -14,7 +14,6 @@ public class HospitalApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
