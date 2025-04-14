afd86f1a714c4374a43692efaedbb543640dd981
Heily Rios <heilyayala005@gmail.com>
2025-04-14 17:26:29 -0500

    Se crearon nuevas excepciones

---
+++ src/autonoma/hospitalapp/exceptions/EnfermedadNoEncontradaException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.hospitalapp.exceptions;
+
+/**
+ *
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ * @since 20250414
+ * @see autonoma.simuladorautomovil.models.Vehiculo
+ * @version 1.0.0
+ */
+public class EnfermedadNoEncontradaException extends RuntimeException{
+
+    public EnfermedadNoEncontradaException(String message) {
+        super(message);
+    }
+    
+}
