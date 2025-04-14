ed1e2eef37325d131ef63808f024a0984d3f3f35
Heily Rios <heilyayala005@gmail.com>
2025-04-14 17:52:09 -0500

    Se añadieron metodos y atributo a las clases inventario y nomina

--- src/autonoma/hospitalapp/models/Inventario.java
+++ src/autonoma/hospitalapp/models/Inventario.java
@@ -6,8 +6,35 @@ package autonoma.hospitalapp.models;
 
 /**
  *
- * @author Maria Paz Puerta
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ * @since 20250414
+ * @see autonoma.simuladorautomovil.models.Vehiculo
+ * @version 1.0.0
  */
 public class Inventario {
+    private String codigo;
+    private int anioActualizacion;
+
+    public Inventario(String codigo, int anioActualizacion) {
+        this.codigo = codigo;
+        this.anioActualizacion = anioActualizacion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getAnioActualizacion() {
+        return anioActualizacion;
+    }
+
+    public void setAnioActualizacion(int anioActualizacion) {
+        this.anioActualizacion = anioActualizacion;
+    }
+    
     
 }
--- src/autonoma/hospitalapp/models/Nomina.java
+++ src/autonoma/hospitalapp/models/Nomina.java
@@ -4,10 +4,51 @@
  */
 package autonoma.hospitalapp.models;
 
+import java.time.LocalDate;
+
 /**
  *
- * @author Maria Paz Puerta
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ * @since 20250414
+ * @see autonoma.simuladorautomovil.models.Vehiculo
+ * @version 1.0.0
  */
 public class Nomina {
+    
+    private String id;
+    private LocalDate fecha;
+    
+    private  double totalPagado;
+
+    public Nomina(String id, LocalDate fecha, double totalPagado) {
+        this.id = id;
+        this.fecha = fecha;
+        this.totalPagado = totalPagado;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getTotalPagado() {
+        return totalPagado;
+    }
+
+    public void setTotalPagado(double totalPagado) {
+        this.totalPagado = totalPagado;
+    }
+    
     
 }
