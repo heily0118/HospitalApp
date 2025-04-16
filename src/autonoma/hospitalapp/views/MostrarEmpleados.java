eb7abcd2e2dcfe8bb0370c061d454086be2a48e8
Heily Rios <heilyayala005@gmail.com>
2025-04-16 00:03:35 -0500

    S creo una nueva ventana

--- src/autonoma/hospitalapp/views/MostrarEmpleados.java
+++ src/autonoma/hospitalapp/views/MostrarEmpleados.java
@@ -31,7 +31,7 @@ public class MostrarEmpleados extends javax.swing.JDialog {
     public MostrarEmpleados(javax.swing.JDialog parent, boolean modal,SistemaCentral sistema) {
         super((Dialog) parent, modal);
         initComponents();
-        setSize(550, 700);
+        setSize(620, 700);
         setResizable(false);
         this.setLocationRelativeTo(null);
         
@@ -62,6 +62,12 @@ public class MostrarEmpleados extends javax.swing.JDialog {
         ListEmpleados = new javax.swing.JScrollPane();
         jScrollPane2 = new javax.swing.JScrollPane();
         ListaEmpleados = new javax.swing.JTable();
+        Actualizar = new javax.swing.JButton();
+        Eliminar = new javax.swing.JButton();
+        Atras = new javax.swing.JButton();
+        MostrarEmpleados = new javax.swing.JButton();
+        btnInformacionEmpleado = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         EmpleadoBuscar = new javax.swing.JTextField();
         btnBuscar = new javax.swing.JPanel();
@@ -71,36 +77,134 @@ public class MostrarEmpleados extends javax.swing.JDialog {
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
+        ListEmpleados.setBackground(new java.awt.Color(255, 255, 255));
+        ListEmpleados.setViewportBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        ListaEmpleados.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         ListaEmpleados.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Nombre", "Documento", "Edad"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        ListaEmpleados.setGridColor(new java.awt.Color(255, 255, 255));
+        ListaEmpleados.setSelectionBackground(new java.awt.Color(255, 255, 255));
         jScrollPane2.setViewportView(ListaEmpleados);
+        if (ListaEmpleados.getColumnModel().getColumnCount() > 0) {
+            ListaEmpleados.getColumnModel().getColumn(0).setResizable(false);
+            ListaEmpleados.getColumnModel().getColumn(1).setResizable(false);
+            ListaEmpleados.getColumnModel().getColumn(2).setResizable(false);
+        }
 
         ListEmpleados.setViewportView(jScrollPane2);
+
+        Actualizar.setBackground(new java.awt.Color(0, 51, 153));
+        Actualizar.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        Actualizar.setForeground(new java.awt.Color(255, 255, 255));
+        Actualizar.setText("Actualizar");
+
+        Eliminar.setBackground(new java.awt.Color(255, 153, 0));
+        Eliminar.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        Eliminar.setForeground(new java.awt.Color(255, 255, 255));
+        Eliminar.setText("Eliminar");
+
+        Atras.setBackground(new java.awt.Color(204, 0, 51));
+        Atras.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        Atras.setForeground(new java.awt.Color(255, 255, 255));
+        Atras.setText("Atras");
+        Atras.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AtrasActionPerformed(evt);
+            }
+        });
+
+        MostrarEmpleados.setBackground(new java.awt.Color(0, 153, 51));
+        MostrarEmpleados.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        MostrarEmpleados.setForeground(new java.awt.Color(255, 255, 255));
+        MostrarEmpleados.setText("Mostrar Empleados");
+
+        btnInformacionEmpleado.setBackground(new java.awt.Color(255, 255, 255));
+        btnInformacionEmpleado.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        btnInformacionEmpleado.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnInformacionEmpleadoMouseClicked(evt);
+            }
+        });
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/autonoma/hospitalapp/images/mostrarInformacionEmpleado.png"))); // NOI18N
+
+        javax.swing.GroupLayout btnInformacionEmpleadoLayout = new javax.swing.GroupLayout(btnInformacionEmpleado);
+        btnInformacionEmpleado.setLayout(btnInformacionEmpleadoLayout);
+        btnInformacionEmpleadoLayout.setHorizontalGroup(
+            btnInformacionEmpleadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, btnInformacionEmpleadoLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(114, 114, 114))
+        );
+        btnInformacionEmpleadoLayout.setVerticalGroup(
+            btnInformacionEmpleadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(btnInformacionEmpleadoLayout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(ListEmpleados, javax.swing.GroupLayout.PREFERRED_SIZE, 360, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(229, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(49, 49, 49)
+                        .addComponent(Atras))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(ListEmpleados, javax.swing.GroupLayout.PREFERRED_SIZE, 360, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(MostrarEmpleados)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(6, 6, 6)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(Eliminar)
+                                            .addComponent(Actualizar)))))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(56, 56, 56)
+                                .addComponent(btnInformacionEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(61, 61, 61)
-                .addComponent(ListEmpleados, javax.swing.GroupLayout.PREFERRED_SIZE, 340, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(166, Short.MAX_VALUE))
+                .addGap(31, 31, 31)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addComponent(btnInformacionEmpleado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(75, 75, 75)
+                        .addComponent(MostrarEmpleados)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(Actualizar)
+                        .addGap(18, 18, 18)
+                        .addComponent(Eliminar))
+                    .addComponent(ListEmpleados, javax.swing.GroupLayout.PREFERRED_SIZE, 340, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(85, 85, 85)
+                .addComponent(Atras)
+                .addContainerGap(79, Short.MAX_VALUE))
         );
 
         jPanel2.setBackground(new java.awt.Color(204, 204, 255));
@@ -165,8 +269,7 @@ public class MostrarEmpleados extends javax.swing.JDialog {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
@@ -213,15 +316,30 @@ public class MostrarEmpleados extends javax.swing.JDialog {
       }
        
     }//GEN-LAST:event_EmpleadoBuscarMouseClicked
+
+    private void AtrasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AtrasActionPerformed
+       this.dispose();
+    }//GEN-LAST:event_AtrasActionPerformed
+
+    private void btnInformacionEmpleadoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnInformacionEmpleadoMouseClicked
+        MostrarInformacionEmpleados mostrarInfo = new MostrarInformacionEmpleados (this,true,sistema);
+        mostrarInfo.setVisible(true);
+    }//GEN-LAST:event_btnInformacionEmpleadoMouseClicked
   
    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Actualizar;
+    private javax.swing.JButton Atras;
+    private javax.swing.JButton Eliminar;
     private javax.swing.JTextField EmpleadoBuscar;
     private javax.swing.JScrollPane ListEmpleados;
     private javax.swing.JTable ListaEmpleados;
+    private javax.swing.JButton MostrarEmpleados;
     private javax.swing.JPanel btnBuscar;
+    private javax.swing.JPanel btnInformacionEmpleado;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane2;
